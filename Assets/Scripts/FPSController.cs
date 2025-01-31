7b80b7ba0a9ff8241aee7207b09264125f8a0004
Szász Balázs <baalak01@gmail.com>
2025-01-31 14:25:49 +0100

    CameraSwitch
    CameraSwitch method init

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -78,7 +78,16 @@ public class FPSController : MonoBehaviour
             FpsCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0,Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
+
+
+        if (Input.GetKey(KeyCode.G))
+        {
+            SwitchCamera(FpsCamera);
+        } 
     }
 
-
+    void SwitchCamera(Camera camera)
+    {
+        
+    }
 }
